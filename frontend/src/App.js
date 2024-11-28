17da2197e7becc06e00b68cb46688d63ea7703a2
ryancondones <ryannjr2004@gmail.com>
2024-11-27 21:19:29 -0500

    Home page UI implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import "./App.css";
 
 function App() {
-  const [inputData, setInputData] = useState({});
+  const [inputData, setInputData] = useState({
+    address: "",
+    beds: "",
+    bath: "",
+    sqft: "",
+  });
   const [prediction, setPrediction] = useState(null);
 
   const handleChange = (e) => {
@@ -26,10 +32,56 @@ function App() {
 
   return (
     <div>
-      <h1>Prediction App</h1>
-      <input type="text" name="feature" onChange={handleChange} />
-      <button onClick={handleSubmit}>Predict</button>
-      {prediction && <p>Prediction: {prediction}</p>}
+      <nav className="navbar">
+        <div className="navbar-title">Home Price Prediction</div>
+          <div className="navbar-links">
+            <a href="home">Home</a>
+            <a href="about">About</a>
+          </div>
+      </nav>
+      <div className="content">
+        <div className="title-panel">
+          <h1>Home Price Predictor</h1>
+          <p className="description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <div className="form-panel">
+          <h2>Fill Out Home Details</h2>
+          <form
+            onSubmit = {(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <div>
+              <label>Address:</label>
+              <input type="text" name="address" value={inputData.address} onChange={handleChange} required/>
+            </div>
+
+            <div>
+              <label>Beds:</label>
+              <input type="number" name="beds" value={inputData.beds} onChange={handleChange} required/>
+            </div>
+
+            <div>
+              <label>Baths:</label>
+              <input type="number" name="baths" value={inputData.baths} onChange={handleChange} required/>
+            </div>
+
+            <div>
+              <label>Square Feet:</label>
+              <input type="number" name="sqft" value={inputData.sqft} onChange={handleChange} required/>
+            </div>
+
+            <button type="submit">Estimate</button>
+          </form>
+          { prediction && <p>Estimated Price: ${prediction}</p> }
+          <p className="disclaimer">
+            Disclaimer: Price estimations have an average error of ~22%.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
